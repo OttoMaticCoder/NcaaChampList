4f44579ea6da4a365628b5853134d9389cb47435
Wesley Otto Jensen <123340921+OttoMaticCoder@users.noreply.github.com>
2024-11-10 08:28:14 -0800

    Add files via upload

---
+++ src/main/java/nattychamp/demo/NattyChampApplication.java
@@ -0,0 +1,13 @@
+package nattychamp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NattyChampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NattyChampApplication.class, args);
+	}
+
+}
